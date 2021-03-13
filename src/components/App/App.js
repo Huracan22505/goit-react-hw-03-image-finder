40cafe5a700177c4e6759b3d3e64825b157d05a2
Huracan <huracan22505@protonmail.com>
2021-03-13 14:38:11 +0200

    init: start project

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import shortid from 'shortid';
+import s from './App.module.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <></>;
+  }
+}
+
+export default App;
